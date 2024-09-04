b2784fff37ba0e33e6640a3b42c75df01b5f1951
TwilCynder <twilcynder@gmail.com>
2024-09-04 03:28:06 +0200

    additional test

--- startgg-helper-base/test/main.js
+++ startgg-helper-base/test/main.js
@@ -1,6 +1,22 @@
 import { makeClient } from "./common.js";
+import { testLong } from "./long.js";
 import { testShort } from "./short.js";
+import { ArgumentsManager } from "@twilcynder/arguments-parser"
+
+let {short, long} = new ArgumentsManager()
+    .addSwitch(["-s", "--short"], {})
+    .addSwitch(["-l", "--long"], {})
+    .enableHelpParameter()
+    .parseProcessArguments()
 
 let client = makeClient();
 
-console.log(await testShort(client));
+if (short){
+    console.log("Testing : single query");
+    console.log(await testShort(client)); 
+}
+
+if (long){
+    console.log("Testing : 100ish queries, with delay-based limiter");
+    console.log(await testLong(client)); 
+}
--- startgg-helper-base/test/main.test.js
+++ startgg-helper-base/test/main.test.js
@@ -1,6 +1,11 @@
 import { makeClient } from "./common.js";
+import { testLong } from "./long.js";
 import { testShort } from "./short";
 
 test("Single query (event results)", async () => {
     expect(await testShort(makeClient())).toBeTruthy();
 });
+
+test("100ish queries with delay-based limiter (sets)", async () => {
+    expect(await testLong(makeClient())).toBeTruthy();
+}, 120000);
