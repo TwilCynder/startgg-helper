2b07cf946c930d4a55e8fce5303305854fd03ffd
TwilCynder <twilcynder@gmail.com>
2024-09-08 17:57:33 +0200

    browser : paginated test

--- browser/test/index.js
+++ browser/test/index.js
@@ -1,1 +1,6 @@
-import {Query} from "./dist/bundle.js"
+import {Query, SGGHelperClient} from "./dist/bundle.js"
+import { testPaginated } from "./testPaginated.js";
+
+let client = new SGGHelperClient("Bearer aa3c0cfb086c57be484694e743b788d03");
+
+console.log(await testPaginated(client));
