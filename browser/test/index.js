d678faf3bbf3a5337ab81560353464aef5e4c01d
TwilCynder <twilcynder@gmail.com>
2024-10-19 00:18:41 +0200

    long ass test

--- browser/test/index.js
+++ browser/test/index.js
@@ -1,4 +1,5 @@
 import {Query, SGGHelperClient} from "./dist/bundle.js"
+import { testLong } from "./testLong.js";
 import { testPaginated } from "./testPaginated.js";
 
 let token;
@@ -18,7 +19,9 @@ if (token){
     let client = new SGGHelperClient(token);
 
     try {
-        await testPaginated(client)
+        await testPaginated(client);
+        await testLong(client);
+        console.log("finito")
     } catch (err){
         console.error("ERROR")
         console.log(err);
--- browser/test/webpack.js
+++ browser/test/webpack.js
@@ -20,5 +20,17 @@ let compiler = webpack({
 });
 
 console.log("Running webpack bundler")
-compiler.run();
-console.log("Bundled npm packages")
+try {
+    await new Promise((resolve, reject) => {
+        compiler.run((err, result) => {
+            if (result.hasErrors()){
+                reject("Error(s) in compilation : " + err + "\nResult : \n" + result.toString())
+            }
+            resolve();
+        })
+    })
+    console.log("Bundled npm packages")
+} catch (err){
+    console.error("Could not compile npm packages : ")
+    console.error(err);
+}
