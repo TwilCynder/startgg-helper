b72146d8322af7d6fc180d860db0bd4b89da9ee1
TwilCynder <twilcynder@gmail.com>
2025-12-27 14:34:50 +0100

    TQS -> TimedSemaphore, now just a simply semaphore with Promises

--- base/src/queryLimiter.js
+++ base/src/queryLimiter.js
@@ -5,7 +5,7 @@
  * 
  * This is implemented like a semaphore automatically releasing tokens after a certain delay
  */
-export class TimedQuerySemaphore {
+export class TimedSemaphore {
     /**
      * @typedef {{client: any, schema: string, 
      *  params: {[varName: string]: value}, 
@@ -44,32 +44,6 @@ export class TimedQuerySemaphore {
     }
 
     /**
-     * @param {any} client 
-     * @param {string} schema 
-     * @param {{[varName: string]: value}} params 
-     * @returns 
-     */
-    #execute(client, schema, params){
-        //console.log("Executing", params);
-
-        this.#startTimeout();
-        
-        return client.request(schema, params);
-
-        /*return new Promise((resolve) => setTimeout(() => {
-            resolve(schema);
-        }, 500))*/
-    }
-
-    /**
-     * @param {Query} query 
-     * @returns 
-     */
-    #executeQuery(query){
-        return this.#execute(query.client, query.schema, query.params);
-    }
-
-    /**
      * Executes the given query with the given GraphQL Client. 
      * Note that this method probably shouldn't be called by anything else than Query.execute
      * @param {any} client 
@@ -77,25 +51,23 @@ export class TimedQuerySemaphore {
      * @param {{[varName: string]: value}} params 
      * @returns 
      */
-    execute(client, schema, params){
+    lock(){
         if (this.#counter > 0){
             this.#counter--;
             //console.log("A ticket was available !", params);
-            return this.#execute(client, schema, params);
+            this.#startTimeout();
+            return;
         } else {
             //console.log("No ticket vailable. Queueing.", params)
-            return new Promise((resolve, reject) =>  this.#queue.push({client, schema, params, resolve, reject}));
+            return new Promise((resolve, reject) =>  this.#queue.push(resolve));
         }
     }
 
     #release(){
         //console.error("Release");
-        let query = this.#queue.shift();
-        if (query){
-            //console.error("A query was queued. Executing :", query.params);
-            this.#executeQuery(query)
-                .then(res => query.resolve(res))
-                .catch(err => query.reject(err));
+        let resFunc = this.#queue.shift();
+        if (resFunc){
+            resFunc();
         } else {
             this.#counter++;
         }
@@ -108,35 +80,5 @@ export class TimedQuerySemaphore {
         for (let timer of this.#timers){
             clearTimeout(timer);
         }
-    }
-}
-
-/**TimedQuerySemaphore that allows one request before waiting. You should use the DelayQueryLimiter instead.*/
-export class ClockQueryLimiter extends TimedQuerySemaphore {
-    /** @param {number} rpm Requests per minute*/
-    constructor(rpm){
-        super(1, 60000 / rpm);
-    }
-}
-
-/** ClockQueryLimiter configured to stay within the start.gg API requests limit (60 per minute, so 1 second delay). You should use the StartGGDelayQueryLimiter instead*/
-export class StartGGClockQueryLimiter extends ClockQueryLimiter {
-    constructor(){
-        super(60);
-    }
-}
-
-/**TimedQuerySemaphore that allows x requests before waiting, and waits for one minute. */
-export class DelayQueryLimiter extends TimedQuerySemaphore {
-    /** @param {number} rpm */
-    constructor(rpm){
-        super (rpm, 60000);
-    }
-}
-
-/** ClockQueryLimiter configured to stay within the start.gg API requests limit (60 per minute)*/
-export class StartGGDelayQueryLimiter extends DelayQueryLimiter {
-    constructor(){
-        super(60)
     }
 }
